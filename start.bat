9d6dc317ca62b31293585c761bbb6ce0690c76a7
EnricoCecchini <enrico.cecchini243@gmail.com>
2024-01-12 00:00:35 -0600

    Requirements.txt for API and update start.bat to create venv and install requirements if venv does not exist

--- start.bat
+++ start.bat
@@ -7,8 +7,26 @@ REM timeout /t 5
 
 REM CD into API directory and start venv in new CMD
 cd API
-call venv/Scripts/activate
 
+REM Check if venv exists, if not create it
+set VENV_DIR=venv
+
+if not exist %VENV_DIR% (
+    python -m venv venv
+
+    call venv/Scripts/activate
+
+    REM Install requirements
+    pip install -r requirements.txt
+
+) else (
+    echo "venv already exists"
+
+    REM activate venv
+    call venv/Scripts/activate
+)
+
+REM Run API
 python main.py
 
 exit
