a6c8020e5bba52394e50ad0ace2ecd9cc3cd2764
EnricoCecchini <enrico.cecchini243@gmail.com>
2024-02-06 19:06:27 -0600

    Start.bat update

--- start.bat
+++ start.bat
@@ -1,9 +1,16 @@
 @echo off
 
+REM Install npm packages if not already installed
+if not exist node_modules (
+    npm ci --quiet
+) else (
+    echo "node_modules already exists"
+)
+
 REM Start new CMD and run Svelte app
 start cmd /k npm run dev
 
-REM timeout /t 5
+timeout /t 5
 
 REM CD into API directory and start venv in new CMD
 cd API
